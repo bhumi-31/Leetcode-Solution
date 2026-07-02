7b941b0138cbf4fb7c8aed567fb342b7b42b292f
Bhumika Narula <146376815+bhumi-31@users.noreply.github.com>
2026-07-02 20:26:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (46.56%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,25 +1,17 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        int startValue = 1;
+        int sum = 0;
+        int minPrefix = 0;
 
-        while(true){
-            int sum = startValue;
-            boolean possible = true;
-            
-            for(int j = 0; j < nums.length; j++){
-                sum = sum + nums[j];
-
-                if(sum < 1){
-                    possible = false;
-                    break;
-                }
-            }
-
-            if(possible){
-                return startValue;
-            }
-
-            startValue++;
+        for(int i = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+            minPrefix = Math.min(minPrefix, sum);
         }
+
+        if(minPrefix < 0){
+            return 1 - minPrefix;
+        }
+
+        return 1;
     }
 }
