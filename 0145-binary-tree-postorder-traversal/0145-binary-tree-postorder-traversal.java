1b5a7a217c0f1e6e5342a2b6881d2b71a021e86d
Bhumika Narula <146376815+bhumi-31@users.noreply.github.com>
2026-07-23 19:36:25 +0530

    Time: 0 ms (100%), Space: 43.3 MB (15.23%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,17 +14,36 @@
  * }
  */
 class Solution {
-    public void postorder(TreeNode root, List<Integer> ans){
-        if(root == null) return;
-
-        postorder(root.left, ans);
-        postorder(root.right, ans);
-        ans.add(root.val);
-    }
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
 
-        postorder(root, ans);
+        if(root == null){
+            return ans;
+        }
+
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+
+        s1.push(root);
+
+        while(!s1.isEmpty()){
+            TreeNode node = s1.pop();
+
+            s2.push(node);
+
+            if(node.left != null){
+                s1.push(node.left);
+            }
+
+            if(node.right != null){
+                s1.push(node.right);
+            }
+        }
+
+        while(!s2.isEmpty()){
+            ans.add(s2.pop().val);
+        }
+
         return ans;
     }
 }
